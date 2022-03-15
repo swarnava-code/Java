1eee7e6c539f614ecb239e4ee75a1b0abf2fe01b
swarnava415 <swarnava415@gmail.com>
2022-03-15 12:55:11 +0530

    first commit

---
+++ src/com/swarnava/java/Main.java
@@ -0,0 +1,9 @@
+package com.swarnava.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("### This application is for practice JAVA.");
+    }
+
+}
